b9bf337f59e54098733d12698563db951b00db0d
WHSD <vasilij.smolkin@yandex.ru>
2023-03-31 00:07:52 +0300

    README.md and Main added

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        SalesManager manager = new SalesManager(new int[]{50,60,1000});
+        System.out.println("Проверяем работу класса SalesManager");
+        System.out.println("Максимум продаж - " + manager.max());
+    }
+}
