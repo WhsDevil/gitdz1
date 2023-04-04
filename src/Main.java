4ed92df368a3c4b4ac00967763f14ae2817ae4db
WHSD <vasilij.smolkin@yandex.ru>
2023-04-04 23:22:24 +0300

    Changed types from int to long

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        SalesManager manager = new SalesManager(new int[]{50,60,1000});
+        SalesManager manager = new SalesManager(new long[]{50,60,1000});
         System.out.println("Проверяем работу класса SalesManager");
         System.out.println("Максимум продаж - " + manager.max());
     }
